15270044ab1ade02a8f52c9126ae634d63ba1899
Jason B <jasonsburke90@gmail.com>
2022-05-01 20:48:55 -0500

    creating posts working

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const neededFunding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const title = document.querySelector('#post-title').value.trim();
+  // const neededFunding = document.querySelector('#project-funding').value.trim();
+  const contents = document.querySelector('#post-contents').value.trim();
 
-  if (name && neededFunding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (title && contents) {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, neededFunding, description }),
+      body: JSON.stringify({ title, contents }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create post');
     }
   }
 };
@@ -26,18 +26,18 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/posts/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete post');
     }
   }
 };
 
-document.querySelector('.new-project-form').addEventListener('submit', newFormHandler);
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
 
-document.querySelector('.project-list').addEventListener('click', delButtonHandler);
+document.querySelector('.post-list').addEventListener('click', delButtonHandler);
