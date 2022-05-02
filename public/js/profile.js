59759c761a6c383e7b5a34491a39ed636f288b39
Jason B <jasonsburke90@gmail.com>
2022-05-01 23:10:07 -0500

    comments for todos

--- public/js/profile.js
+++ public/js/profile.js
@@ -41,3 +41,5 @@ const delButtonHandler = async (event) => {
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
 
 document.querySelector('.post-list').addEventListener('click', delButtonHandler);
+
+// add replies queryselector
